60b361d55cbe3c0e570dd1107a02e5baad1c36fc
busypeoples <busypeoples@gmail.com>
2014-02-07 20:22:27 +0100

    Added karma config

---
+++ karma.conf.js
@@ -0,0 +1,73 @@
+// Karma configuration
+// Generated on Fri Feb 07 2014 19:57:20 GMT+0100 (CET)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'components/angular/angular.min.js',
+      'components/angular-resource/angular-resource.min.js',
+      'components/angular-mocks/angular-mocks.js',
+      'components/angular-route/angular-route.min.js',
+      'src/*.js',
+      'test/*.test.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      'components/'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
