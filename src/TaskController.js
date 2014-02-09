605c510034d6df42d83f73f1f2c81ebfc5912656
busypeoples <busypeoples@gmail.com>
2014-02-09 11:38:41 +0100

    Added TaskController for adding / editing tasks

---
+++ src/TaskController.js
@@ -0,0 +1,62 @@
+var Module = angular.module('timebox');
+
+Module.controller('TaskController', ['$scope', '$routeParams', 'Tasks', 'TaskService', '$location', function($scope, $routeParams, Tasks, TaskService, $location) {
+
+	$scope.saveTask = saveTask;
+	$scope.deleteTask = deleteTask;
+
+	init();
+
+	function init() {
+		if (! $routeParams.id) $location.path('/tasks');
+
+		$scope.addMode = ($routeParams.id == 'add')? true : false;
+		$scope.task= Tasks.get($routeParams.id);
+
+		initDatePicker();
+	}
+
+	function initDatePicker() {
+		// Disable weekend selection
+		$scope.disabled = function(date, mode) {
+			return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+		};
+
+		$scope.toggleMin = function() {
+			$scope.minDate = ( $scope.minDate ) ? null : new Date();
+		};
+		$scope.toggleMin();
+
+		$scope.open = function($event) {
+			$event.preventDefault();
+			$event.stopPropagation();
+
+			$scope.opened = true;
+		};
+
+		$scope.dateOptions = {
+			'year-format': "'yy'",
+			'starting-day': 1
+		};
+	}
+
+	function saveTask(task) {
+		if (task.id) {
+			Tasks.saveTask(task);
+		} else {
+			Tasks.addTask(task);
+		}
+
+		$location.path('/tasks');
+	}
+
+	function deleteTask(task) {
+		Tasks.removeTask(task);
+		$location.path('/tasks');
+	}
+
+	function validate(task) {
+
+	}
+
+}]);
