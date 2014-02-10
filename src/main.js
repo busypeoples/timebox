ed1abca07033c8981238e963c3a3eb767dd4db10
busypeoples <busypeoples@gmail.com>
2014-02-10 21:46:39 +0100

    Changed view script paths

--- src/main.js
+++ src/main.js
@@ -7,23 +7,23 @@ var Module = angular.module('timebox', ['ngResource', 'ngRoute','ui.bootstrap'])
 Module.config(['$routeProvider', function($routeProvider){
 	$routeProvider
 		.when('/', {
-			templateUrl : '/timeboxApp/view/dashboard.html',
+			templateUrl : '/view/dashboard.html',
 			controller : 'DashboardController'
 		})
 		.when('/users', {
-			templateUrl : '/timeboxApp/view/users.html',
+			templateUrl : '/view/users.html',
 			controller : 'UserController'
 		})
 		.when('/tasks', {
-			templateUrl : '/timeboxApp/view/tasks.html',
+			templateUrl : '/view/tasks.html',
 			controller : 'TasksController'
 		})
 		.when('/task/:id?', {
-			templateUrl : '/timeboxApp/view/task.html',
+			templateUrl : 'view/task.html',
 			controller : 'TaskController'
 		})
 		.when('/settings', {
-			templateUrl : '/timeboxApp/view/settings.html',
+			templateUrl : '/view/settings.html',
 			controller : 'SettingsController'
 		});
 }]);
