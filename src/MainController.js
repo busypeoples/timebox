cd42e16bddbf223116e84a237dba69743c3fb17f
busypeoples <busypeoples@gmail.com>
2014-02-10 22:56:39 +0100

    Updated task time handling inside navigation bar.

--- src/MainController.js
+++ src/MainController.js
@@ -3,11 +3,11 @@
 !function(angular) {
 	var Module = angular.module('timebox');
 
-	Module.controller('MainController', ['$scope', '$location', 'Tasks',function($scope, $location, Tasks) {
+	Module.controller('MainController', ['$scope', '$location', 'Tasks', 'TimerService', function($scope, $location, Tasks, TimerService) {
 
 		$scope.isActive = isActive;
 		$scope.$watch(activeChanged, setActive);
-
+		$scope.$watch(TimerService.getTime, updateTime);
 
 		function activeChanged() {
 			return $scope.currentTask !== Tasks.getActive();
@@ -19,6 +19,10 @@
 		
 		function isActive(location) {
 			return location === $location.path();
+		}
+		
+		function updateTime() {
+			$scope.formattedTime = TimerService.getFormattedTime();
 		}
 
 	}]);
