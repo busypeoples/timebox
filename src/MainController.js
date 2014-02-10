d5926def1e2b451868d036deec359e1262305882
busypeoples <busypeoples@gmail.com>
2014-02-10 21:54:19 +0100

    Updated Navigation
    -- Set active li in navigation

--- src/MainController.js
+++ src/MainController.js
@@ -3,9 +3,11 @@
 !function(angular) {
 	var Module = angular.module('timebox');
 
-	Module.controller('MainController', ['$scope', 'Tasks',function($scope, Tasks) {
+	Module.controller('MainController', ['$scope', '$location', 'Tasks',function($scope, $location, Tasks) {
 
+		$scope.isActive = isActive;
 		$scope.$watch(activeChanged, setActive);
+
 
 		function activeChanged() {
 			return $scope.currentTask !== Tasks.getActive();
@@ -13,6 +15,10 @@
 
 		function setActive() {
 			$scope.currentTask = Tasks.getActive();
+		}
+		
+		function isActive(location) {
+			return location === $location.path();
 		}
 
 	}]);
